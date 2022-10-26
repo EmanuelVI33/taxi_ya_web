ebc7d2760043c8bf33ca961489fe14e486ff43e9
Dario S <dsuarezlazarte@gmail.com>
2022-10-26 15:47:18 -0400

    xd

--- app/Models/Vehiculo.php
+++ app/Models/Vehiculo.php
@@ -8,13 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Vehiculo extends Model
 {
     use HasFactory;
-<<<<<<< HEAD
-    
-=======
     //metodo para recibir la foreing key
     public function user()
     {
         return $this->belongsTo(Conductor::class);
     }
->>>>>>> ecd25ad3b605f54d5711b8df6c5378b21db6b859
 }
